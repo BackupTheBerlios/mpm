71da7c51ab316344747672ac7453abdfc2d8018a
ivop <ivop>
2006-09-25 09:14:08 +0000

    better way to filter out environment from .pc files
    strip all lines starting with a capital letter and those starting with
    a # (comments)
    I haven't come accross any environment variables starting with a capital
    letter. Is that legal in .pc files?

--- pmpkg-config/pmpkg-config.sh
+++ pmpkg-config/pmpkg-config.sh
@@ -196,7 +196,8 @@ all_requires() {
 data_from_file() {
     field=$1
     file=$2
-    env=`cat $file | sed -e '/^[ ]*$/q'`
+#    env=`cat $file | sed -e '/^[ ]*$/q'`
+    env=`sed -e '/^[#ABCDEFGHIJKLMNOPQRSTUVWXYZ]\+.*/d' $file`
     flags=`cat $file | grep "^$field:" | cut -d ':' -f 2`
     output=`eval "eval $env"; eval echo $flags`
     echo $output
