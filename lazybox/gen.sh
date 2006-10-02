0aed2291add8b09462ccf83c9a046d5627d4a26c
ivop <ivop>
2006-10-02 16:16:29 +0000

    show what you are doing

--- lazybox/gen.sh
+++ lazybox/gen.sh
@@ -7,11 +7,15 @@
 > elseif.h
 > allnames.h
 
+echo -n "gen.mak: "
+
 echo "LINKS = \\" >> gen.mak
 for i in $SOURCES ; do
     echo "    $i \\" >> gen.mak
 done
 echo "    $CPLINKS" >> gen.mak
+
+echo -n "(sources) "
 
 echo "OBJS = \\" >> gen.mak
 for i in $SOURCES ; do
@@ -19,12 +23,22 @@ for i in $SOURCES ; do
 done
 echo "" >> gen.mak
 
+echo -n "(objects) "
+
 for i in $SOURCES ; do
     cat >> gen.mak <<__EOF__
 $i.c:
 	@./getsource.sh \${SOURCEPATH}/$i.c
 
 __EOF__
+done
+
+echo -n "(dependencies) "
+echo "ok "
+
+echo -n "header files: "
+
+for i in $SOURCES ; do
     j=`echo $i | tr '.' '_'`
     if test "$i" = "$j" ; then
         echo "REGISTER($i);" >> register.h
@@ -39,5 +53,11 @@ for i in $CPLINKS ; do
     echo "ELSEIF2($i, cp);" >> elseif.h
 done
 
+echo -n "register.h elseif.h "
+
 echo "#define" ALLNAMES \"$SOURCES $CPLINKS\" >> allnames.h
+
+echo -n "allnames.h "
+
+echo ""
 
