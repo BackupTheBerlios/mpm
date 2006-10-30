acdd812e90e77673539b5d069a25e299c6cecbeb
ivop <ivop>
2006-10-30 11:51:16 +0000

    update and clean up
    bootdisk now only conatins bootblock and image, using only 3 i-nodes which saves
    another 32kB
    the root filesystem is completely encapsulated by the kernel image (imgrd)
    code to generate such a kernel image will follow shortly

--- mpmports/BOOTDISK/mk.fd0.sh
+++ mpmports/BOOTDISK/mk.fd0.sh
@@ -7,7 +7,7 @@ curdir=`pwd`
 dev=/dev/fd0
 boot=/usr/src/boot/boot
 bootblock=/usr/src/boot/bootblock
-image=/boot/image/3.1.2a
+image=work/kernelimage
 makeboot=/usr/src/boot
 
 error() {
@@ -21,8 +21,8 @@ read ans
 test "$ans" != "y" -a "$ans" != "yes" && exit 2
 
 if test ! -f $boot -o ! -f $bootblock ; then
-    echo "]]] Making bootloader and bootblock"
-    test -f $makeboot/Makefile || error "cannot find system sources"
+    echo "*** Making bootloader and bootblock"
+    test -f $makeboot/Makefile || error "cannot find system sources ($makeboot)"
     cd $makeboot
     make
     cd $curdir
@@ -30,56 +30,32 @@ fi
 
 test -f $image || error "cannot find kernel image"
 
-echo "]]] Making filesystem on $dev"
-mkfs -B 1024 -b 1440 -i 512 $dev
+echo "*** Unmounting $dev"
+umount $dev || true
 
-echo "]]] Mounting $dev"
+echo "*** Making filesystem on $dev"
+mkfs -B 1024 -b 1440 -i 3 $dev
+
+echo "*** Mounting $dev"
 mount $dev /mnt
 
-echo "]]] Creating directory structure"
-cd /mnt
-mkdir -p boot/image bin etc dev sbin
-
-echo "]]] Installing $boot"
-cp -p $boot /mnt/boot/boot
+echo "*** Installing $boot to /mnt/boot"
+cp -p $boot /mnt/boot
 sync
 
-echo "]]] Installing $image"
-cp -p $image /mnt/boot/image/image
+echo "*** Installing $image to /mnt/image"
+cp -p $image /mnt/image
 sync
 
-echo "]]] Creating devices"
-cd /mnt/dev
-/usr/bin/MAKEDEV std
-sync
-
-echo "]]] Installing files to /bin"
-for i in /bin/sh /bin/printroot /usr/bin/df ; do
-    j=`basename $i`
-    cp $i /mnt/bin
-    sync
-    echo -n "$j "
-done
-echo
-
-echo "]]] Installing dummy rc file"
-cat > /mnt/etc/rc << __EOF__
-echo "Hello, world"
-/bin/printroot > /etc/mtab
-/bin/df
-echo "Goodbye, cruel world"
-__EOF__
-
-echo "]]] Unmounting $dev"
-cd $curdir
+echo "*** Unmounting $dev"
 umount $dev
 
-echo "]]] Installing bootblock"
-installboot -d $dev $bootblock /boot/boot
+echo "*** Installing bootblock"
+installboot -d $dev $bootblock /boot
 
-echo "]]] Setting boot options"
-edparams $dev 'rootdev=ram; ramimagedev=fd0; save'
+echo "*** Setting boot options"
 edparams $dev 'leader(){echo PRESS ESC FOR PROMPT}; save'
 edparams $dev 'main(){delay 2000; boot}; save'
 
-echo "]]] Done"
+echo "*** Done"
+df $dev
