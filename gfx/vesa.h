422ec066b7ee4189ba7d3a12137fc6227bf5c21b
ivop <ivop>
2006-11-20 11:14:55 +0000

    vesa info struct

---
+++ gfx/vesa.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2006, Ivo van Poorten
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * My name may NOT be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef VESA_H
+#define VESA_H
+
+typedef unsigned char  BYTE;
+typedef unsigned short WORD;
+typedef unsigned long  POINTER;
+
+/* vesa 1.2 */
+typedef struct vesa_info_s {
+    BYTE        VESASignature[4];
+    WORD        VESAVersion;
+    POINTER     OEMStringPtr;
+    BYTE        Capabilities[4];
+    POINTER     VideoModePtr;
+    WORD        TotalMemory;
+    BYTE        Reserved[236];
+} vesa_info_t;
+
+#endif
