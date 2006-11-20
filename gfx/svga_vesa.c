d480753a9c7dc1cdb084bba5576f4540ca41fd22
ivop <ivop>
2006-11-20 19:48:25 +0000

    detect whether VBE is available or not

--- gfx/svga_vesa.c
+++ gfx/svga_vesa.c
@@ -48,6 +48,7 @@
 #include "gfx.h"
 #include "gfx_ioctl.h"
 #include "generic.h"
+#include "vesa.h"
 
 EXTERN int debug;
 
@@ -58,10 +59,12 @@ PRIVATE char *myname;
 PRIVATE struct reg86u reg86;
 PRIVATE unsigned short width = 0, height = 0;
 PRIVATE unsigned char bpp = 0;
+PRIVATE vesa_info_t vesa;
 
 PRIVATE int init(char *name) {
     struct reg86u reg86;
     int r;
+    char tmp[80];
 
     myname = name;
 
@@ -69,12 +72,41 @@ PRIVATE int init(char *name) {
     if (r != OK) panic(myname, "sys_getbiosbuffer failed", r);
     DEBUG report(myname, "bios_buf_vir", bios_buf_vir);
     DEBUG report(myname, "bios_buf_size", bios_buf_size);
-    r = sys_umap(SYSTEM, D, bios_buf_vir, (phys_bytes)bios_buf_size,
-                                                            &bios_buf_phys);
+    r = sys_umap(SYSTEM, D, bios_buf_vir, bios_buf_size, &bios_buf_phys);
     if (r != OK) panic(myname, "sys_umap failed", r);
     if (bios_buf_phys + bios_buf_size > 0x100000)
         panic(myname, "bad BIOS buffer, phys", bios_buf_phys);
     DEBUG report(myname, "bios_buf_phys", bios_buf_phys);
+
+    /* get svga info */
+    reg86.u.w.ax = 0x4f00;
+    reg86.u.w.es = bios_buf_phys / HCLICK_SIZE;
+    reg86.u.w.di = bios_buf_phys % HCLICK_SIZE;
+    reg86.u.b.intno = 0x10;
+    r = sys_int86(&reg86);
+    if (r != OK) panic(myname, "unable to retrieve SVGA information", r);
+
+    r = sys_vircopy(SYSTEM, D, bios_buf_vir,
+                    SELF, D, (vir_bytes) &vesa, sizeof(vesa) );
+    if (r != OK) panic(myname, "sys_vircopy failed", r);
+
+    DEBUG {
+        snprintf(tmp, 80, "Signature: '%c%c%c%c'",
+            vesa.VESASignature[0], vesa.VESASignature[1],
+            vesa.VESASignature[2], vesa.VESASignature[3]);
+        report(myname, tmp, NO_NUM);
+        snprintf(tmp, 80, "Version: %i.%i",
+            vesa.VESAVersion>>8, vesa.VESAVersion & 0xff);
+        report(myname, tmp, NO_NUM);
+        snprintf(tmp, 80, "TotalMemory: %i kB", vesa.TotalMemory * 64);
+        report(myname, tmp, NO_NUM);
+    }
+
+    if (strncmp((char*)vesa.VESASignature, "VESA", 4) != 0)
+        panic(myname, "no VESA BIOS support found", NO_NUM);
+
+    if (vesa.VESAVersion < 0x0100)
+        panic(myname, "need at least VESA 1.0", NO_NUM);
 
     current_mode = GFX_MODE_NONE;
     return 0;
